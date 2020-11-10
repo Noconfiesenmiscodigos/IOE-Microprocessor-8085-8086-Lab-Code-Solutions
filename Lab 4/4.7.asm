8caf9d00e7eea71af14fdbf7dbc8ea0453a9e0cb
e^iπ <58982771+Noconfiesenmiscodigos@users.noreply.github.com>
2020-11-10 12:19:40 +0545

    Add files via upload

---
+++ Lab 4/4.7.asm
@@ -0,0 +1,23 @@
+;Write a program to rotate the data 3C in port A. Call a delay subroutine for the delaying.
+
+MVI B,3CH
+MVI D,08H
+
+LOOP: MOV A,B
+RLC
+NOP
+OUT 40H
+CC L1
+MOV B,A
+L2: DCR D
+MOV A,D
+CPI 00H
+JNC L3
+
+L1: INR C
+RET
+
+L3: JZ L4
+JNZ LOOP
+
+L4: HLT 
